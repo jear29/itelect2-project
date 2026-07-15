b60b48ea3226d6cfc791ae32100a0ff6ed8252f7
John Ray A. Ferriol <ferriol.john29@gmail.com>
2026-07-15 15:11:54 +0800

    GT3: Add ES6+ utility functions

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 // app.js - Main application entry point
+import { formatDate, validateTask, mergeTaskUpdate } from './utils.js';
+
 console.log('Server starting...');
+
+console.log(formatDate(new Date("2026-07-22")));
+
+console.log(validateTask({ title: 'Title', dueDate: new Date("2026-07-22") }));
+
+console.log(mergeTaskUpdate({ title: 'Title' }, { title: 'New Title' }));
